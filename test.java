9c26eb23e7adc59f41bd16d218cd4367193ee073
zhuzijie <2672884775@qq.com>
2025-01-03 18:49:32 +0800

    add test.java

---
+++ test.java
@@ -0,0 +1,111 @@
+public class test {
+
+    // 选择排序
+    public static void selectionSort(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                minIndex = arr[j] < arr[minIndex] ? j : minIndex;
+            }
+            swap(arr, i, minIndex);
+        }
+    }
+
+    // 冒泡排序
+    public static void bubbleSort(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        for (int e = arr.length - 1; e > 0; e--) {
+            for (int i = 0; i < e; i++) {
+                if (arr[i] > arr[i + 1]) {
+                    swap(arr, i, i + 1);
+                }
+            }
+        }
+    }
+
+    // 插入排序
+    public static void insertionSort(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        for (int i = 1; i < arr.length; i++) {
+            for (int j = i - 1; j >= 0 && arr[j] > arr[j + 1]; j--) {
+                swap(arr, j, j + 1);
+            }
+        }
+    }
+
+    // 交换数组中两个元素的位置
+    public static void swap(int[] arr, int i, int j) {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
+
+    // 返回一个数组，数组长度随机[0,maxLen-1]，数组元素随机[0,maxValue-1]
+    public static int[] lenRandomValueRandom(int maxLen, int maxValue) {
+        int len = (int) (Math.random() * maxLen);
+        int[] arr = new int[len];
+        for (int i = 0; i < len; i++) {
+            arr[i] = (int) (Math.random() * maxValue);
+        }
+        return arr;
+    }
+
+    // 拷贝一个数组
+    public static int[] copyArray(int[] arr) {
+        if (arr == null) {
+            return null;
+        }
+        int[] res = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            res[i] = arr[i];
+        }
+        return res;
+    }
+
+    public static boolean isEqual(int[] arr1, int[] arr2) {
+        if ((arr1 == null && arr2 != null) || (arr1 != null && arr2 == null)) {
+            return false;
+        }
+        if (arr1 == null) {
+            return true;
+        }
+        if (arr1.length != arr2.length) {
+            return false;
+        }
+        for (int i = 0; i < arr1.length; i++) {
+            if (arr1[i] != arr2[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    public static void main(String[] args) {
+        int maxLen = 10;
+        int maxValue = 1000;
+        int testTime = 10000;
+        for (int i = 0; i < testTime; i++) {
+            int[] arr = lenRandomValueRandom(maxLen, maxValue);
+            int[] arr1 = copyArray(arr);
+            int[] arr2 = copyArray(arr);
+            selectionSort(arr1);
+            bubbleSort(arr2);
+            if (!isEqual(arr1, arr2)) {
+                for (int k : arr) {
+                    System.out.print(k + " ");
+                }
+                System.out.println("Error");
+                break;
+            }
+        }
+    }
+
+}
